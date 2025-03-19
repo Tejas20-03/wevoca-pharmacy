7c6f5a71a70b3508873df7044f93a42e529bd79a
tejaschhabra.official <tejaschhabra.official@gmail.com>
2025-03-19 09:19:56 +0000

    Submenu Font Update

--- src/containers/categories-menu/categories-menu/categories-menu-iitem.tsx
+++ src/containers/categories-menu/categories-menu/categories-menu-iitem.tsx
@@ -1,63 +1,73 @@
-import { Accordion, AccordionSummary, AccordionDetails, Typography, Button } from '@mui/material';
-import { GetCategoryMenuData } from '@/services/categories/types';
-import Link from 'next/link';
-import React, { useState } from 'react';
-import DownArrow from '@/components/Global-Icon/down-arrow';
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Button,
+} from "@mui/material";
+import { GetCategoryMenuData } from "@/services/categories/types";
+import Link from "next/link";
+import React, { useState } from "react";
+import DownArrow from "@/components/Global-Icon/down-arrow";
 
 interface IProps {
-    item: GetCategoryMenuData;
+  item: GetCategoryMenuData;
 }
 
 const renderSubMenu = (subMenu: GetCategoryMenuData[]) => {
-    return (
-        <ul className="submenu">
-            {subMenu.map((item, index) => (
-                <li key={`${item}_${index}`}>
-                    <AccordionItem item={item} />
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul className="submenu">
+      {subMenu.map((item, index) => (
+        <li key={`${item}_${index}`}>
+          <AccordionItem item={item} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 const AccordionItem: React.FC<IProps> = ({ item }) => {
-    const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
-    const toggleAccordion = () => {
-        setExpanded(!expanded);
-    };
-    const unableToToggleAccordion = () => {
-        setExpanded(false);
-    };
+  const toggleAccordion = () => {
+    setExpanded(!expanded);
+  };
+  const unableToToggleAccordion = () => {
+    setExpanded(false);
+  };
 
-    const hasSubMenu = item.SubMenu && item.SubMenu.length > 0;
-    const accordionClass = hasSubMenu ? 'cateogries-menu' : 'cateogries-menu no-submenu';
+  const hasSubMenu = item.SubMenu && item.SubMenu.length > 0;
+  const accordionClass = hasSubMenu
+    ? "cateogries-menu"
+    : "cateogries-menu no-submenu";
 
-    return (
-        <Accordion className={accordionClass} expanded={expanded} onChange={hasSubMenu ? toggleAccordion : unableToToggleAccordion}>
-            <AccordionSummary className="cateogries-itemLink">
-                <Typography>
-                    <Link href={`/cat/${item.Slug}`}>{item.Name}</Link>
-                </Typography>
-                <Button onClick={toggleAccordion}>
-                    {hasSubMenu && (
-                        <DownArrow color={'--primary-color-darker'} />
-                    )}
-                </Button>
-            </AccordionSummary>
-            <AccordionDetails>
-                {hasSubMenu && renderSubMenu(item.SubMenu)}
-            </AccordionDetails>
-        </Accordion>
-    );
+  return (
+    <Accordion
+      className={accordionClass}
+      expanded={expanded}
+      onChange={hasSubMenu ? toggleAccordion : unableToToggleAccordion}
+    >
+      <AccordionSummary className="cateogries-itemLink">
+        <Typography>
+          <Link href={`/cat/${item.Slug}`}>{item.Name}</Link>
+        </Typography>
+        <Button onClick={toggleAccordion}>
+          {hasSubMenu && <DownArrow color={"--primary-color-darker"} />}
+        </Button>
+      </AccordionSummary>
+      <AccordionDetails>
+        {hasSubMenu && renderSubMenu(item.SubMenu)}
+      </AccordionDetails>
+    </Accordion>
+  );
 };
 
 const CategoriesMenuItem: React.FC<IProps> = ({ item }) => {
-    return (
-        <>
-            <AccordionItem item={item} />
-        </>
-    );
+  return (
+    <>
+      <AccordionItem item={item} />
+    </>
+  );
 };
 
 export default CategoriesMenuItem;
--- src/containers/header/header-index/header-desktop/category-menu/category-menu.tsx
+++ src/containers/header/header-index/header-desktop/category-menu/category-menu.tsx
@@ -1,41 +1,59 @@
-import DownArrow from '@/components/Global-Icon/down-arrow';
-import RightArrow from '@/components/Global-Icon/Right-arrow';
-import { GetCategoryMenuData } from '@/services/categories/types';
-import Link from 'next/link';
-import React from 'react';
-import style from './category-menu.module.scss'
+import DownArrow from "@/components/Global-Icon/down-arrow";
+import RightArrow from "@/components/Global-Icon/Right-arrow";
+import { GetCategoryMenuData } from "@/services/categories/types";
+import Link from "next/link";
+import React from "react";
+import style from "./category-menu.module.scss";
 
 interface IProps {
-    item: GetCategoryMenuData;
+  item: GetCategoryMenuData;
 }
 
-const renderSubMenu = (subMenu: GetCategoryMenuData[], hasSubSubMenu = false, item: string) => {
-    return (
-        <ul className={`submenu ${hasSubSubMenu ? `has-sub-submenu` : ''} ${item}`}>
-            {subMenu.map((item, index) => (
-                <li key={`${item}_${index}`}>
-                    <Link href={`/cat/${item.Slug}`}>
-                        {item.Name} {item.SubMenu?.length > 0 && <RightArrow />}
-                    </Link>
-                    {item.SubMenu?.length > 0 && renderSubMenu(item.SubMenu, item.SubMenu.some(subItem => subItem.SubMenu?.length > 0))}
-                </li>
-            ))}
-        </ul>
-    );
+const renderSubMenu = (
+  subMenu: GetCategoryMenuData[],
+  hasSubSubMenu = false,
+  item: string
+) => {
+  return (
+    <ul className={`submenu ${hasSubSubMenu ? `has-sub-submenu` : ""} ${item}`}>
+      {subMenu.map((item, index) => (
+        <li key={`${item}_${index}`}>
+          <Link
+            href={`/cat/${item.Slug}`}
+            className={` ${style.subtitleText} secondary-font`}
+          >
+            {item.Name} {item.SubMenu?.length > 0 && <RightArrow />}
+          </Link>
+          {item.SubMenu?.length > 0 &&
+            renderSubMenu(
+              item.SubMenu,
+              item.SubMenu.some((subItem) => subItem.SubMenu?.length > 0)
+            )}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 const CategoryMenu: React.FC<IProps> = ({ item }) => {
-    return (
-        <>
-            <li>
-                <Link href={`/cat/${item.Slug}`}>
-                    {item.Name} {item.SubMenu?.length > 0 && <DownArrow />}
-                </Link>
-                {item.SubMenu?.length > 0 && renderSubMenu(item.SubMenu, item.SubMenu.some(subItem => subItem.SubMenu?.length > 0), item.Slug)}
-            </li>
-        </>
-    );
+  return (
+    <>
+      <li>
+        <Link
+          href={`/cat/${item.Slug}`}
+          className={` ${style.titleText} secondary-font`}
+        >
+          {item.Name} {item.SubMenu?.length > 0 && <DownArrow />}
+        </Link>
+        {item.SubMenu?.length > 0 &&
+          renderSubMenu(
+            item.SubMenu,
+            item.SubMenu.some((subItem) => subItem.SubMenu?.length > 0),
+            item.Slug
+          )}
+      </li>
+    </>
+  );
 };
-
 
 export default CategoryMenu;
